abdfa446de32cc179329c35fc4a5a0830dcd6d48
Tudor Groza <tudor.groza@pryzm.health>
2019-10-22 15:57:37 +0800

    Fixed UI deployment script.

--- ui-deployment/build_deploy_ui.sh
+++ ui-deployment/build_deploy_ui.sh
@@ -4,5 +4,5 @@ docker build --force-rm=true -t gwas-deposition-ui:latest .
 docker tag gwas-deposition-ui:latest ebispot/gwas-deposition-ui:latest-sandbox
 docker push ebispot/gwas-deposition-ui:latest-sandbox
 
-kubectl --kubeconfig ~/.kube/gwas-depo-embassy-dev-config.yml delete -f gwas-deposition-ui-deployment.yaml
-kubectl --kubeconfig ~/.kube/gwas-depo-embassy-dev-config.yml create -f gwas-deposition-ui-deployment.yaml
+kubectl --kubeconfig ~/.kube/gwas-depo-embassy-dev-config.yml delete deploy gwas-deposition-ui -n gwas
+kubectl --kubeconfig ~/.kube/gwas-depo-embassy-dev-config.yml apply -f gwas-deposition-ui-deployment.yaml
