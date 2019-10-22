1c8a0a64af23424d97be9c22c8fe40b884543cf5
Tudor Groza <tudor.groza@pryzm.health>
2019-10-22 15:32:53 +0800

    Fixed NodePort issue for UI deployment.

--- ui-deployment/build_deploy_ui.sh
+++ ui-deployment/build_deploy_ui.sh
@@ -4,5 +4,5 @@ docker build --force-rm=true -t gwas-deposition-ui:latest .
 docker tag gwas-deposition-ui:latest ebispot/gwas-deposition-ui:latest-sandbox
 docker push ebispot/gwas-deposition-ui:latest-sandbox
 
-kubectl --kubeconfig ~/.kube/gwas-depo-embassy-dev-config.yml delete deploy gwas-deposition-ui
+kubectl --kubeconfig ~/.kube/gwas-depo-embassy-dev-config.yml delete -f gwas-deposition-ui-deployment.yaml
 kubectl --kubeconfig ~/.kube/gwas-depo-embassy-dev-config.yml create -f gwas-deposition-ui-deployment.yaml
